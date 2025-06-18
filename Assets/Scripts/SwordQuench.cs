2a47e07b1c42c2f06163829c50bfbb7f835259c5
Retrocowboy6343 <136213529+Retrocowboy6343@users.noreply.github.com>
2025-06-19 09:53:24 +1200

    finished sword quenching code (i think)

--- Assets/Scripts/SwordQuench.cs
+++ Assets/Scripts/SwordQuench.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SwordQuench : MonoBehaviour
@@ -6,26 +8,28 @@ public class SwordQuench : MonoBehaviour
     //variables
     public Material coolMaterial;
     public Material hotMaterial;
+    private bool isCool;
     [SerializeField] private int cooldownTime;
-    
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    {       
+        OnObjectSpawn();
+        isCool = false;
+    }   
 
     void OnObjectSpawn()
     {
-        
+        CooldownTimer();
+        meshRenderer.materials[0] = hotMaterial;
     }
-
+    
     IEnumerator CooldownTimer()
     {
         yield return new WaitForSeconds(cooldownTime);
@@ -33,6 +37,14 @@ public class SwordQuench : MonoBehaviour
     }
     private void OnCooldown()
     {
-        meshRenderer.material = coolMaterial;
+        meshRenderer.materials[0] = coolMaterial;
+        isCool = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Qbucket"))
+        {
+            OnCooldown();
+        }
     }
 }
