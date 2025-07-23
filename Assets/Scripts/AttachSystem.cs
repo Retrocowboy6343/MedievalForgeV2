2e0553b59003af30f1bba95e3eba39cc0fc493e5
Retrocowboy6343 <136213529+Retrocowboy6343@users.noreply.github.com>
2025-07-23 16:15:24 +1200

    ATTACH SYSTEM WORKS!!!

--- Assets/Scripts/AttachSystem.cs
+++ Assets/Scripts/AttachSystem.cs
@@ -5,19 +5,34 @@ using UnityEngine.XR.Interaction.Toolkit.Transformers;
 public class AttachSystem : MonoBehaviour
 {
     public Transform hiltAttachpoint;
+    public int dettachVelocity;
+    public GetHammerSpeed getHammerSpeed;
+
+
+
+    private void Start()
+    {
+        getHammerSpeed = FindFirstObjectByType<GetHammerSpeed>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Blade"))
         {
-            other.GetComponent<XRGrabInteractable>().enabled = false;
-            other.GetComponent<Rigidbody>().Sleep();
+            //other.GetComponent<XRGrabInteractable>().enabled = false;
+            //other.GetComponent<Rigidbody>().Sleep();
             other.GetComponent<Rigidbody>().isKinematic = true;
             other.GetComponent<BoxCollider>().enabled = false;
-            other.GetComponent<XRBaseGrabTransformer>().enabled = false;
+            //other.GetComponent<XRBaseGrabTransformer>().enabled = false;
             other.gameObject.transform.position = hiltAttachpoint.position;
             other.gameObject.transform.parent = transform;
-            other.gameObject.transform.rotation = Quaternion.identity;
+            other.gameObject.transform.localEulerAngles = new Vector3(90, 0, 0);
+        }
+        if (other.gameObject.CompareTag("Hammer") && getHammerSpeed.hammerYVelocity > dettachVelocity)
+        {
+            transform.SetParent(null);
+            other.GetComponent<BoxCollider>().enabled = true;
+
         }
     }
 }
